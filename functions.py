6c52a084cc854d32e9e5ba852d81f0265269fd0b
adarsh0610 <47912152+adarsh0610@users.noreply.github.com>
2019-03-09 12:23:03 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,19 @@
+def func():
+	print("hiii")
+	print("hello I'm Adarsh")
+func()
+def func1(a):
+	print("hey","Adarsh\t"+a)
+func1("hiii")
+def func2(b,c):
+	s=b+c
+	print(b,"+",c,"=",s)
+func2(2,8)
+def func3(d,e):
+	print(d,e)
+func3(2,8)
+def func4(college="CMRU"):
+	print("I am in ",college)
+func4("IITM")
+func4("IITD")
+func4()
